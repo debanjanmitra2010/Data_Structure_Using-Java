a2bbb95c960ece29b0a6577adfebaa57cd210e1e
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-01-12 19:52:17 -0500

    Add files via upload

---
+++ CircularLinkedList/Operations.java
@@ -0,0 +1,13 @@
+public interface Operations {
+
+    public Node CreatCLL(int NodeValue);
+    public void insertInCLL(int nodeValue, int position);
+    public void traversal();
+    public void search(int NodeValue);
+    public void deleteNode(int location);
+    public void deleteCLL();
+    public void printScreen();
+    public void Continue();
+
+}
+
