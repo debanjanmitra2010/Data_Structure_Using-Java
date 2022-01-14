7c52ddbc3a3fc8dca25ca8f334f692e07b726e34
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-01-13 23:35:10 -0500

    Create Main.java

---
+++ DoublyLinkedList/Main.java
@@ -0,0 +1,15 @@
+public class Main3 {
+
+    public static void main(String[] args) {
+        DoubleLinkedList dll = new DoubleLinkedList();
+        dll.createDLL(1);
+        System.out.print("Do you want to create a LinkedList (Y/N): ");
+        char choice = s.next().charAt(0);
+        if (choice == 'Y' | choice == 'y') {
+            System.out.println("Enter your 1st Element in LinkedList");
+            int n = s.nextInt();
+            cll.CreatCLL(n);
+            cll.printScreen();
+        }
+    }
+}
