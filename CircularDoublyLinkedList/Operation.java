81949afdcf6433f9b106c4113d307b00d1067e9b
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-01-14 21:27:17 -0500

    Create Operation.java

---
+++ CircularDoublyLinkedList/Operation.java
@@ -0,0 +1,20 @@
+public interface Operation {
+
+    public DoublyNode CreatCDLL(int NodeValue);
+
+    public void insertInCDLL(int nodeValue, int position);
+
+    public void traversal();
+
+    public void ReverseTraversal();
+
+    public void search(int NodeValue);
+
+    public void deleteNode(int location);
+
+    public void deleteCDLL();
+
+    public void printScreen();
+
+    public void Continue();
+}
