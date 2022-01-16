b422cb181a165ea765f7743802aa07d75132f9d7
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-01-16 17:37:16 -0500

    Update Main.java

--- CircularLinkedList/Main.java
+++ CircularLinkedList/Main.java
@@ -6,8 +6,8 @@ public class Main {
         Scanner s = new Scanner(System.in);
         CLL cll = new CLL();
         System.out.print("Do you want to create a LinkedList (Y/N): ");
-        char choice = s.next().charAt(0);
-        if (choice == 'Y' | choice == 'y') {
+        char choice = s.next().toLowerCase().charAt(0);
+        if (choice == 'y') {
             System.out.println("Enter your 1st Element in LinkedList");
             int n = s.nextInt();
             cll.CreatCLL(n);
