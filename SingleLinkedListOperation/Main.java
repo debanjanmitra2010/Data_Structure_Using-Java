116837ae5be21529044ca7eaef995e0eefca05ca
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-01-16 17:38:17 -0500

    Update Main.java

--- SingleLinkedListOperation/Main.java
+++ SingleLinkedListOperation/Main.java
@@ -5,8 +5,8 @@ public class Main {
         Scanner s = new Scanner(System.in);
         SingleLinkedList SLL = new SingleLinkedList();
         System.out.printf("Do you want to create a LinkedList (Y/N): ");
-        char choice = s.next().charAt(0);
-        if (choice == 'Y' | choice == 'y') {
+        char choice = s.next().toLowerCase().charAt(0);
+        if (choice == 'y') {
             System.out.println("Enter your 1st Element in LinkedList");
             int n = s.nextInt();
             SLL.createSingleLinkedin(n);
