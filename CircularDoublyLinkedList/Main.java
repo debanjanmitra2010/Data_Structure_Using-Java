ba92304cd65244e16fb8290e00a1a48482ff904f
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-01-16 17:37:30 -0500

    Update Main.java

--- CircularDoublyLinkedList/Main.java
+++ CircularDoublyLinkedList/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner s = new Scanner(System.in);
         CLL cll = new CLL();
         System.out.print("Do you want to create a Circular Doubly LinkedList (Y/N): ");
-        char choice = s.next().toLowerCase()charAt(0);
+        char choice = s.next().toLowerCase().charAt(0);
         if (choice == 'y') {
             System.out.println("Enter your 1st Element in CDLL");
             int n = s.nextInt();
