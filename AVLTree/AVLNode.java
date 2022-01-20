f188ab999310a71db79f0184478b458b4b020d68
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-01-19 22:34:56 -0500

    Create AVLNode.java

---
+++ AVLTree/AVLNode.java
@@ -0,0 +1,10 @@
+public class AVLNode {
+    public int value;
+    public AVLNode left;
+    public AVLNode right;
+    public int height;
+
+    public AVLNode() {
+        this.height = 0;
+    }
+}
