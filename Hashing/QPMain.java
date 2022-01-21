32bc3569980684788ec84c1484c69b0ea9aed40d
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-01-21 10:56:31 -0500

    Create QPMain.java

---
+++ Hashing/QPMain.java
@@ -0,0 +1,13 @@
+public class QPMain {
+    public static void main(String[] args) {
+        QuadraticProbing quadraticProbing = new QuadraticProbing(13);
+        quadraticProbing.insertKeyInHashTable("The");
+        quadraticProbing.insertKeyInHashTable("quick");
+        quadraticProbing.insertKeyInHashTable("brown");
+        quadraticProbing.insertKeyInHashTable("fox");
+        quadraticProbing.insertKeyInHashTable("over");
+        quadraticProbing.insertKeyInHashTable("lazy");
+        quadraticProbing.displayHashTable();
+
+    }
+}
