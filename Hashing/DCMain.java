fc9c343f6effaa123fe4a08f8b92915c1129ccc6
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-01-21 10:50:55 -0500

    Create DCMain.java

---
+++ Hashing/DCMain.java
@@ -0,0 +1,14 @@
+public class DCMain {
+    public static void main(String[] args) {
+        DirectChaining directChaining = new DirectChaining(13);
+        directChaining.insertHashTable("The");
+        directChaining.insertHashTable("quick");
+        directChaining.insertHashTable("brown");
+        directChaining.insertHashTable("fox");
+        directChaining.insertHashTable("over");
+        directChaining.deleteKeyHashTable("ox");
+        directChaining.displayHashTable();
+
+    }
+}
+
